afd1d1e5ce9c88c2302aac24a0d2460e33347df6
Riyadh <riyadhahmed324@gmail.com>
2024-06-13 14:10:36 -0400

    final commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const field = document.getElementById("input-field")
+const output = document.getElementById("input-fieldtwo")
+
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+function genPass()
+{
+
+    const randomLength1 = Math.floor(Math.random()*10) + 1
+    const randomLength2 = Math.floor(Math.random()*10) + 1
+
+    let randomSelection1 = '';
+    for(let i = 0; i<characters.length; i++){
+
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomSelection1 += characters[randomIndex];
+    }
+
+    let randomSelection2 = '';
+    for(let i = 0; i<characters.length; i++){
+
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomSelection2 += characters[randomIndex];
+    }
+
+    field.value = randomSelection1;
+    output.value = randomSelection2;
+
+}
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("generatebtn").addEventListener("click", genPass);
+});
+
+function clearAll(){
+    output.value = ''
+    field.value = ''
+}
